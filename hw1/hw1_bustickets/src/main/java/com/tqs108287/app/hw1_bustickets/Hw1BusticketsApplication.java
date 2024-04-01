017a13ddc82e8511e354e6037dd289c0a2b0d571
Migas77 <miguel.belchior@ua.pt>
2024-04-01 11:43:59 +0100

    Hopefully github actions + sonar cloud working

--- hw1/hw1_bustickets/src/main/java/com/tqs108287/app/hw1_bustickets/Hw1BusticketsApplication.java
+++ hw1/hw1_bustickets/src/main/java/com/tqs108287/app/hw1_bustickets/Hw1BusticketsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Hw1BusticketsApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(Hw1BusticketsApplication.class, args);
 	}
